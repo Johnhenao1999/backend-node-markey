200bf78bffa780d7827aeea3d6ee26357d385b9b
John Angel Henao <john.alvarez@correounivalle.edu.co>
2023-02-25 15:20:22 -0500

    Se sube proyecto backend del aplicativo Markey Confecciones

---
+++ routes.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const routes = express.Router();
+
+
+// Mostrar todos los atributos de los empleados
+routes.get('/holamundo', (req, res)=>{
+     req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('SELECT *  FROM empleados', (err, rows)=>{
+            if(err) return res.send(err)
+            res.json(rows);
+        })
+    }) 
+})
+// Mostar ciertos atributos de los empleados
+routes.get('/holamundoprueba', (req, res)=>{
+    req.getConnection((err, conn)=>{
+       if(err) return res.send(err)
+       conn.query('SELECT apellido, email FROM empleados', (err, rows)=>{
+           if(err) return res.send(err)
+           res.json(rows);
+       })
+   }) 
+})
+
+
+//Metodo para consultar a un usuario por ID
+routes.get('/holamundo/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        const id = req.params.id
+       if(err) return res.send(err)
+       conn.query("SELECT * FROM empleados WHERE id=' "+id+"'", (err, rows)=>{
+           if(err) return res.send(err)
+           res.json(rows);
+       })
+   }) 
+})
+
+
+// Metodo para guardar un usuario en la base de datos
+
+routes.post('/holamundo', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('INSERT INTO  empleados set ?', [req.body], (err, rows)=>{
+            if(err) return res.send(err)
+            res.json('Guardado correctamente en la base de datos');
+        });
+    });
+});
+
+//Metodo para eliminar un usuario de la base de datos
+routes.delete('/holamundo/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('DELETE FROM empleados WHERE id = ?', [req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+            res.json('Eliminado correctamente en la base de datos');
+        });
+    });
+});
+
+//Metodo para actualizar un usuario de la base de datos
+routes.put('/holamundo/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('UPDATE empleados set ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+            res.json('Actualizado correctamente en la base de datos');
+        });
+    });
+});
+
+// Metodo para guardar una maquina en la base de datos
+
+routes.post('/insertar_maquinas', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('INSERT INTO  gestion_inventario set ?', [req.body], (err, rows)=>{
+            if(err) return res.send(err)
+            res.json('Guardado correctamente en la base de datos');
+        });
+    });
+}); 
+
+// Mostrar todos los atributos del inventario
+routes.get('/maquinas', (req, res)=>{
+    req.getConnection((err, conn)=>{
+       if(err) return res.send(err)
+       conn.query('SELECT *  FROM gestion_inventario', (err, rows)=>{
+           if(err) return res.send(err)
+           res.json(rows);
+       })
+   }) 
+})  
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql2')
+const myconn = require('express-myconnection')
+const cors = require('cors')
+
+const routes = require('./routes');
+
+app.set('port', 8000);
+
+const dbOptions ={
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'jhonHenao123456',
+    database: 'gestion_usuario'
+} 
+
+app.use(myconn(mysql, dbOptions, 'single'))
+app.use(express.json());
+app.use(cors())
+
+app.use('/', routes);
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
