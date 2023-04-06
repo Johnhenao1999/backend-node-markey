e74f5227bdebd63ca17c392b638a3828b4233e0d
John Angel Henao <john.alvarez@correounivalle.edu.co>
2023-04-06 16:19:25 -0500

    Se realiza el ajuste en la ruta empleados para poder consultar empleado por id y realizar la actualizacion de la informacion desde el fron

--- routes/empleados/empleados.js
+++ routes/empleados/empleados.js
@@ -23,8 +23,21 @@ routes.get('/empleados', (req, res) => {
     })
 });
 
+/* ------------------ METODO PARA CONSULTAR A EMPLEADO POR ID ------------------ */
+routes.get('/empleados/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        const id = req.params.id
+        if (err) return res.send(err)
+        conn.query("SELECT * FROM empleados_markey WHERE idcedula=' " + id + "'", (err, rows) => {
+            if (err) return res.send(err)
+            res.json(rows);
+        })
+    })
+})
+
+
 /*------------------  ELIMINAR EMPLEADO EN LA BASE DE DATOS ---------------- */
-routes.delete('/holamundo/:id', (req, res) => {
+routes.delete('/empleados/:id', (req, res) => {
     req.getConnection((err, conn) => {
         if (err) return res.send(err)
         conn.query('DELETE FROM empleados_markey WHERE idcedula = ?', [req.params.id], (err, rows) => {
@@ -35,27 +48,16 @@ routes.delete('/holamundo/:id', (req, res) => {
 });
 
 //Metodo para actualizar un usuario de la base de datos
-routes.put('/holamundo/:id', (req, res) => {
+routes.put('/empleados/:id', (req, res) => {
     req.getConnection((err, conn) => {
         if (err) return res.send(err)
-        conn.query('UPDATE empleados set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+        conn.query('UPDATE empleados_markey set ? WHERE idcedula = ?', [req.body, req.params.id], (err, rows) => {
             if (err) return res.send(err)
             res.json('Actualizado correctamente en la base de datos');
         });
     });
 });
 
-//Metodo para consultar a un usuario por ID
-routes.get('/holamundo/:id', (req, res) => {
-    req.getConnection((err, conn) => {
-        const id = req.params.id
-        if (err) return res.send(err)
-        conn.query("SELECT * FROM empleados WHERE id=' " + id + "'", (err, rows) => {
-            if (err) return res.send(err)
-            res.json(rows);
-        })
-    })
-})
 
 // Mostar ciertos atributos de los empleados
 routes.get('/holamundoprueba', (req, res) => {
@@ -64,17 +66,6 @@ routes.get('/holamundoprueba', (req, res) => {
         conn.query('SELECT apellido, email FROM empleados', (err, rows) => {
             if (err) return res.send(err)
             res.json(rows);
-        });
-    });
-});
-
-//Metodo para eliminar un usuario de la base de datos
-routes.delete('/ingreso_empleados/:id', (req, res) => {
-    req.getConnection((err, conn) => {
-        if (err) return res.send(err)
-        conn.query('DELETE FROM ingreso_empleados WHERE idingreso = ?', [req.params.id], (err, rows) => {
-            if (err) return res.send(err)
-            res.json('Registro eliminado correctamente de la base de datos');
         });
     });
 });
--- routes/inventario/maquinaria.js
+++ routes/inventario/maquinaria.js
@@ -3,10 +3,10 @@ const routes = express.Router()
 
 // Metodo para guardar una maquina en la base de datos
 
-routes.post('/insertar_maquinas', (req, res) => {
+routes.post('/ingresar-maquinaria', (req, res) => {
     req.getConnection((err, conn) => {
         if (err) return res.send(err)
-        conn.query('INSERT INTO  gestion_inventario set ?', [req.body], (err, rows) => {
+        conn.query('INSERT INTO gestion_maquinaria set ?', [req.body], (err, rows) => {
             if (err) return res.send(err)
             res.json('Guardado correctamente en la base de datos');
         });
@@ -17,12 +17,33 @@ routes.post('/insertar_maquinas', (req, res) => {
 routes.get('/maquinas', (req, res) => {
     req.getConnection((err, conn) => {
         if (err) return res.send(err)
-        conn.query('SELECT *  FROM gestion_inventario', (err, rows) => {
+        conn.query('SELECT *  FROM gestion_maquinaria', (err, rows) => {
             if (err) return res.send(err)
             res.json(rows);
-        })
-    })
-})
+        });
+    });
+});
 
+//Metodo para actualizar un usuario de la base de datos
+routes.put('/maquina/:id_maquina', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('UPDATE gestion_maquinaria set ? WHERE id_maquina = ?', [req.body, req.params.id], (err, rows) => {
+            if (err) return res.send(err)
+            res.json('Actualizado correctamente en la base de datos');
+        });
+    });
+});
+
+//Metodo para eliminar un usuario de la base de datos
+routes.delete('/maquina/:id_maquina', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('DELETE FROM gestion_maquinaria WHERE id_maquina = ?', [req.params.id], (err, rows) => {
+            if (err) return res.send(err)
+            res.json('Registro eliminado correctamente de la base de datos');
+        });
+    });
+});
 module.exports = routes;
  
