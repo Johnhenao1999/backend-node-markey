9a54cdb62a308d3ad8ce32b6930ed6c8f0b5775a
John Angel Henao <john.alvarez@correounivalle.edu.co>
2023-04-19 15:11:23 -0500

    Se realizan ajustes

--- routes/clientes/clientes.js
+++ routes/clientes/clientes.js
@@ -36,12 +36,24 @@ routes.get('/clientes/:id', (req, res) => {
     })
 })
 
+/* ------------------ METODO PARA CONSULTAR A CLIENTE POR ID ------------------ */
+routes.put('/clientes/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('UPDATE clientes set ? WHERE id_cliente = ?', [req.body, req.params.id], (err, rows) => {
+            if (err) return res.send(err)
+            res.json('Actualizado correctamente en la base de datos');
+        });
+    });
+});
+
+
 
 /*------------------  ELIMINAR CLIENTE EN LA BASE DE DATOS ---------------- */
 routes.delete('/clientes/:id', (req, res) => {
     req.getConnection((err, conn) => {
         if (err) return res.send(err)
-        conn.query('DELETE FROM clientes WHERE idcliente = ?', [req.params.id], (err, rows) => {
+        conn.query('DELETE FROM clientes WHERE id_cliente = ?', [req.params.id], (err, rows) => {
             if (err) return res.send(err)
             res.json('Eliminado correctamente en la base de datos');
         });
